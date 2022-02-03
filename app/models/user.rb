69a69d3dc58dca61b4706901c427c4587d8ec8e6
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-03 15:43:04 +0900

    バリデーションの設定２

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,20 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :birth, presence: true
-  validates :first_name_kan, presence: true
-  validates :family_name_kan, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kan
+    validates :family_name_kan
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name_kan
+    validates :family_name_kan
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 
   has_many :items
   has_many :purchases
