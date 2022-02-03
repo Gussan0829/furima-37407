ad8e6f6526e6812080d2bdeca396a9ff51e98eff
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-03 15:46:13 +0900

    アソシエーションの削除

--- db/migrate/20220202064801_devise_create_users.rb
+++ db/migrate/20220202064801_devise_create_users.rb
@@ -10,7 +10,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name_kana,    null:false
       t.string :family_name_kana,   null:false
       t.date   :birth,              null: false
-      t.string :email,              null: false, unique:true
+      t.string :email,              null: false
       t.string :encrypted_password, null: false
 
       ## Recoverable
