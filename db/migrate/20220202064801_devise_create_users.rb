0c51a4bb4133733ea98428d2a25b72c4809131be
Gussan0829 <97212006+Gussan0829@users.noreply.github.com>
2022-02-03 11:20:49 +0900

    マイグレーションファイルの改変　users

--- db/migrate/20220202064801_devise_create_users.rb
+++ db/migrate/20220202064801_devise_create_users.rb
@@ -4,14 +4,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null:false, default: ""
-      t.string :first_name_kan,     null:false, default: ""
-      t.string :family_name_kan,    null:false, unique:true, default: ""
-      t.string :first_name_kana,    null:false, default: ""
-      t.string :family_name_kana,   null:false, default: ""
-      t.string :birth,              null: false, default: ""
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null:false
+      t.string :first_name_kan,     null:false
+      t.string :family_name_kan,    null:false
+      t.string :first_name_kana,    null:false
+      t.string :family_name_kana,   null:false
+      t.date   :birth,              null: false
+      t.string :email,              null: false, unique:true
+      t.string :encrypted_password, null: false
 
       ## Recoverable
       t.string   :reset_password_token
